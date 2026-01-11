40c5da84b0338c22021d2b209b30c059841d58d4
Debra Mugwe <debramugwe@gmail.com>
2026-01-11 11:56:36 -0800

    added components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -9,7 +12,13 @@ function App() {
 
   return (
     <>
+
       <WelcomeMessage />    
+      <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
