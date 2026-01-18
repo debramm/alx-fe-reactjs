27b6526b5013db906ce8034b7a640a4f2a35071d
Debra Mugwe <debramugwe@gmail.com>
2026-01-18 11:52:07 -0800

    added alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f4f4f4', padding: '15px', minHeight: '200px' }}>
+      <p style={{ fontSize: '18px', textAlign: 'center' }} >I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
