f2ee8896e49d8f98572d8103280203112d60edb7
Debra Mugwe <debramugwe@gmail.com>
2026-01-18 11:14:37 -0800

    added styling to components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,11 +7,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
     <UserProfile 
   name="Alice" 
@@ -42,6 +44,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <Counter />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
