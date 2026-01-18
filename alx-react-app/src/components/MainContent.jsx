396dc6c1271fae00c65a15438d63ed8c7808ed5a
Debra Mugwe <debramugwe@gmail.com>
2026-01-18 11:38:19 -0800

    removed styling

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
-    <main style={{ backgroundColor: '#f4f4f4', padding: '15px', minHeight: '200px' }}>
-      <p style={{ fontSize: '18px', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
+    <main >
+      <p >I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
