4d2bdf58122bdd61126576b134c950b55296e355
Debra Mugwe <debramugwe@gmail.com>
2026-01-26 09:51:43 -0700

    added recipe management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,8 +2,10 @@
 
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import DeleteRecipeButton from './components/DeleteRecipeButton';
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
 import './App.css'
-
 
 function App() {
   
@@ -12,6 +14,9 @@ function App() {
     <h1>Recipe App</h1>
       <AddRecipeForm />
       <RecipeList />
+      <DeleteRecipeButton/>
+      <RecipeDetails/>
+      <EditRecipeForm/>
     
       
     </>
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -8,9 +8,16 @@ const useRecipeStore = create((set) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  setRecipes: (recipes) =>
-    set(() => ({
-      recipes,
+ deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  setRecipes: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      )
     })),
 }));
 
