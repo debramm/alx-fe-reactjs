d36508c93dc1acf959f0832c74e4d2538fb88210
Debra Mugwe <debramugwe@gmail.com>
2026-02-08 11:10:54 -0900

    Added Advanced Interactivity and Transitions with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,6 +7,9 @@ function UserProfile() {
         max-w-xs md:max-w-sm
         mx-auto my-20
         rounded-lg shadow-lg
+        shadow-lg
+        hover:shadow-xl
+        transition-shadow duration-300 ease-in-out
       "
     >
       <img
@@ -17,6 +20,8 @@ function UserProfile() {
           rounded-full
           sm:w-24 sm:h-24
           md:w-36 md:h-36
+          hover:scale-110
+          transition-transform duration-300 ease-in-out
         "
       />
 
@@ -26,6 +31,8 @@ function UserProfile() {
           text-center
           sm:text-lg md:text-xl
           text-blue-800
+          hover:text-blue-500
+          transition-colors duration-300 ease-in-out
         "
       >
         John Doe
