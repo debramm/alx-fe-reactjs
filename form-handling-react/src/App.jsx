08ef73e67d79aa346af0ee413da7987d0db3870f
Debra Mugwe <debramugwe@gmail.com>
2026-02-20 08:49:30 -0800

    fix formik file

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import RegistrationForm from "./components/RegistrationForm.jsx";
-import FormikForm from "./components/FormikForm.jsx";
+import FormikForm from "./components/formikForm.";
 
 function App() {
   return (
